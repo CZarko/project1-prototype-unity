fdebce6b12047b3c6b1a896e988f9fc444fb69b9
JoeFrumenti <111709603+jfrument@users.noreply.github.com>
2022-10-05 14:04:12 -0700

    Made most of nose game
    waiting for sam to fix the rocket

---
+++ FeverDream/Assets/Code/Scripts/Nose/Rocket.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private float rotateSpeed = 180f;
+    private float rocketSpeed = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        transform.Rotate(new Vector3(0,0,rotateSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal")));
+        //GetComponent<Rigidbody>().AddForce
+        //transform.Translate(new Vector3(-1 * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.z),Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z), 0) * Time.deltaTime);
+        //print(Mathf.Cos(transform.eulerAngles.z * Mathf.PI / 180));
+        transform.Translate(Vector3.up * Time.deltaTime * rocketSpeed);
+    }
+}
