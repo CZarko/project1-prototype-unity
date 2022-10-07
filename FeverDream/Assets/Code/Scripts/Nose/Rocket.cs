2c4e2b568ab65ab96f1f663bb0dc48f9de9fd079
JoeFrumenti <111709603+jfrument@users.noreply.github.com>
2022-10-07 12:33:18 -0700

    Finished nose game except the slime is gray

--- FeverDream/Assets/Code/Scripts/Nose/Rocket.cs
+++ FeverDream/Assets/Code/Scripts/Nose/Rocket.cs
@@ -15,10 +15,15 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        transform.Rotate(new Vector3(0,0,rotateSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal")));
+        transform.Rotate(new Vector3(0, -1 * rotateSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal"), 0));
         //GetComponent<Rigidbody>().AddForce
         //transform.Translate(new Vector3(-1 * Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.z),Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z), 0) * Time.deltaTime);
         //print(Mathf.Cos(transform.eulerAngles.z * Mathf.PI / 180));
-        transform.Translate(Vector3.up * Time.deltaTime * rocketSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * rocketSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
     }
 }
